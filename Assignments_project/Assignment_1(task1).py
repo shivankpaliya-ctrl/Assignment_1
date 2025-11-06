5db46c2faffd00353ed21bb4f71c79fbbeac403a
shivankpaliya-ctrl <shivankpaliya@gmail.com>
2025-11-06 13:40:11 +0530

    Add files via upload

---
+++ Assignments_project/Assignment_1(task1).py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print("Addtion: ",(a + b))
+print("Subtraction: ",(a - b))
+print("Multiplication: ",(a * b))
+print("Division: ",(a / b))
+
---
+++ Assignments_project/Assignment_1(task2).py
@@ -0,0 +1,6 @@
+a = str(input("Enter your first name: "))
+b = str(input("Enter your second name: "))
+
+result = str(a+" "+b)
+print("Hello,",result,"!Welcome to the Python program.")
+
